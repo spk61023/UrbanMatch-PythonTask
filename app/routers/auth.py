6df5db2c4cc27ef94fb198662b2bf2a1eeaa29df
Samarth Kulkarni <spk61023@gmail.com>
2024-08-05 18:17:14 +0530

    Adds docs for auth

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -24,8 +24,36 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 SECRET_KEY = secrets.token_urlsafe(32)
 ALGORITHM = "HS256"
 
+"""
+This module contains the authentication routes for the FastAPI application.
+Functions:
+- login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(database.get_session)): 
+	Handles the login functionality. Verifies the user's credentials and returns an access token.
+- get_user(db: Session, username: str): 
+	Retrieves a user from the database based on the provided username.
+- get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(database.get_session)): 
+	Retrieves the current user based on the provided access token.
+- create_access_token(data: dict, expires_delta: timedelta = None): 
+	Creates an access token with the provided data and expiration time.
+"""
+
+
+# Login route
 @router.post("/auth/login", response_model=Token)
 def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(database.get_session)):
+	"""
+	Endpoint for user authentication and login.
+
+	Parameters:
+	- form_data (OAuth2PasswordRequestForm): The form data containing the username and password for login.
+	- db (Session): The database session.
+
+	Returns:
+	- Token: The access token and token type.
+
+	Raises:
+	- HTTPException: If the username or password is incorrect or if an error occurs during login.
+	"""
 	try:
 		user = db.query(User).filter(User.username == form_data.username).first()
 		if not user or not user.verify_password(form_data.password):
@@ -47,6 +75,19 @@ def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depend
 		)
     
 def get_user(db: Session, username: str):
+	"""
+	Retrieve a user from the database by their username.
+
+	Args:
+		db (Session): The database session.
+		username (str): The username of the user to retrieve.
+
+	Returns:
+		User: The user object if found, None otherwise.
+
+	Raises:
+		HTTPException: If an error occurs while retrieving the user.
+	"""
 	try:
 		return db.query(User).filter(User.username == username).first()
 	except Exception as e:
@@ -57,31 +98,55 @@ def get_user(db: Session, username: str):
 		)
 
 async def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(database.get_session)):
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get("sub")
-        if username is None:
-            raise credentials_exception
-        token_data = TokenData(username=username)
-    except JWTError:
-        raise credentials_exception
-    user = get_user(db, username=token_data.username)
-    if user is None:
-        raise credentials_exception
-    return user
+	"""
+	Get the current user based on the provided token.
+
+	Parameters:
+	- token (str): The authentication token.
+	- db (Session): The database session.
+
+	Returns:
+	- user: The current user.
+
+	Raises:
+	- HTTPException: If the credentials cannot be validated.
+	"""
+	credentials_exception = HTTPException(
+		status_code=status.HTTP_401_UNAUTHORIZED,
+		detail="Could not validate credentials",
+		headers={"WWW-Authenticate": "Bearer"},
+	)
+	try:
+		payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+		username: str = payload.get("sub")
+		if username is None:
+			raise credentials_exception
+		token_data = TokenData(username=username)
+	except JWTError:
+		raise credentials_exception
+	user = get_user(db, username=token_data.username)
+	if user is None:
+		raise credentials_exception
+	return user
 
 
 def create_access_token(data: dict, expires_delta: timedelta = None):
-    to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.now() + expires_delta
-    else:
-        expire = datetime.now() + timedelta(minutes=15)
-    to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
+	"""
+	Generates an access token using the provided data and expiration delta.
+
+	Parameters:
+		data (dict): The data to be encoded in the access token.
+		expires_delta (timedelta, optional): The expiration delta for the access token. If not provided, a default expiration of 15 minutes will be used.
+
+	Returns:
+		str: The generated access token.
+
+	"""
+	to_encode = data.copy()
+	if expires_delta:
+		expire = datetime.now() + expires_delta
+	else:
+		expire = datetime.now() + timedelta(minutes=15)
+	to_encode.update({"exp": expire})
+	encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+	return encoded_jwt
